424ae17d749e5cbc3f70c7b33082582badb32386
ArielAyala <arielayalamail@gmail.com>
2021-03-27 15:40:12 -0300

    Lib for http requests

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import Http from '../../libs/http';
 
 class CoinsScreen extends Component {
 
+    componentDidMount = async () => {
+        const coins = await Http.instance.get('https://api.coinlore.net/api/tickers/');
+
+        console.log('coins', coins)
+    }
+
     handlePress = () => {
-        console.log('go to detail', this.props)
+        // console.log('go to detail', this.props)
 
         this.props.navigation.navigate("CoinDetail");
     }
