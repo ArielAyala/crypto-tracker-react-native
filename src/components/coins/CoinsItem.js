81c0117b2083f412c43d331cc4615737c462f95c
ArielAyala <arielayalamail@gmail.com>
2021-03-30 21:02:44 -0400

    Add image to detail page and sectionlist

--- src/components/coins/CoinsItem.js
+++ src/components/coins/CoinsItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet,Pressable, Image, Platform } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image, Platform } from 'react-native';
 import Colors from '../../res/colors';
 
 const CoinsItem = (props) => {
@@ -38,6 +38,7 @@ const styles = StyleSheet.create({
         padding: 16,
         borderBottomColor: Colors.zircon,
         borderBottomWidth: 1,
+        paddingLeft: Platform.OS == 'ios' ? 0 : 16,
         marginLeft: Platform.OS == 'ios' ? 16 : 0
     },
     row: {
