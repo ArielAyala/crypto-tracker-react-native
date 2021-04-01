1cda0833b2dc11977687875b072f96700b0eda4a
ArielAyala <arielayalamail@gmail.com>
2021-04-01 10:34:26 -0400

    Adding firt tab

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from "react";
-import {NavigationContainer} from '@react-navigation/native';
+import { Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import CoinsStack from './src/components/coins/CoinsStack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Colors from "./src/res/colors";
 
-const App =() => {
+const Tabs = createBottomTabNavigator();
+
+const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor:'#fefefe',
+          style: {
+            backgroundColor: Colors.blackPearl
+          }
+        }}
+      >
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (<Image
+              style={{ tintColor:color ,width: size, height: size }}
+              source={require('./src/assets/bank.png')}
+            />
+            )
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
